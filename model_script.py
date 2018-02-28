e8542b8ccae3b59833c7c6c58756f3d360c20fe0
Alex Rich <alexandersrich@gmail.com>
2018-02-28 09:56:51 -0500

    update modeling script to train model for each user
    trains one linear model on all users, and then a separate one for each user,
    using predictions from first. Also standardize parameter scales. Still not as
    good as lightgbm, but trying to get closer using linear model

--- model_script.py
+++ model_script.py
@@ -3,51 +3,91 @@ import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
-import lightgbm as lgb
+# import lightgbm as lgb
+import numpy as np
+from scipy import sparse
 
 
 # load data
-data = build_data('data/data_en_es/en_es.slam.20171218.train.new',
+data = build_data('en', 'data/data_en_es/en_es.slam.20171218.train.new',
                   'data/data_en_es/en_es.slam.20171218.dev.new',
-                  'data/data_en_es/en_es.slam.20171218.dev.key', n_users=None)
-train_x, train_ds, train_y, dev_x, dev_ids, dev_y = data
+                  'data/data_en_es/en_es.slam.20171218.dev.key', n_users=300)
+train_x, train_ids, train_y, dev_x, dev_ids, dev_y = data
+
+# store user id's separately
+train_users = np.array([d.pop('user') for d in train_x])
+dev_users = np.array([d.pop('user') for d in dev_x])
 
 # put data in scipy sparse matrix
 dv = DictVectorizer()
 train_x_sparse = dv.fit_transform(train_x)
 dev_x_sparse = dv.transform(dev_x)
 
+# standardize predictor scales
+scalable_preds = [i for i in dv.feature_names_ if '=' not in i]
+scalable_preds_idx = [dv.vocabulary_[i] for i in scalable_preds]
+pred_ranges = (train_x_sparse[:, scalable_preds_idx].max(axis=0).A -
+               train_x_sparse[:, scalable_preds_idx].min(axis=0).A).flatten()
+for i in range(len(pred_ranges)):
+    if pred_ranges[i] == 0:
+        pred_ranges[i] = 1
+for i in range(len(pred_ranges)):
+    train_x_sparse[:, scalable_preds_idx[i]] = (
+        train_x_sparse[:, scalable_preds_idx[i]] / pred_ranges[i]
+    )
+    dev_x_sparse[:, scalable_preds_idx[i]] = (
+        dev_x_sparse[:, scalable_preds_idx[i]] / pred_ranges[i]
+    )
+
+print('data shape:', train_x_sparse.shape)
+
 # train linear model
-model = LogisticRegression(C=.1)
+print('training all-users linear')
+model = LogisticRegression(C=.5)
 model.fit(train_x_sparse, train_y)
+train_df = model.decision_function(train_x_sparse)
+dev_df = model.decision_function(dev_x_sparse)
 dev_predicted = model.predict_proba(dev_x_sparse)[:, 1]
 dev_predictions_df = pd.DataFrame({
     'instance': dev_ids,
     'prediction': dev_predicted
 })
 # print auc score
-print(roc_auc_score(dev_y, dev_predicted))
-dev_predictions_df.to_csv('sklearn_logreg.pred',
-                          header=False, index=False, sep=" ")
+print('all-users auc:', roc_auc_score(dev_y, dev_predicted))
 
-# train light gradient boosting machine model
-d_train = lgb.Dataset(train_x_sparse, label=train_y)
-d_valid = lgb.Dataset(dev_x_sparse, label=dev_y)
-params = {
-    'application': 'binary',
-    'metric': 'auc',
-    'learning_rate': .1,
-    'num_leaves': 128,
-    'min_data_in_leaf': 20,
-}
-bst = lgb.train(params, d_train, valid_sets=[d_train, d_valid],
-                valid_names=['train', 'valid'],
-                num_boost_round=1000, verbose_eval=10)
-dev_predicted = bst.predict(dev_x_sparse)
-# print auc score
-print(roc_auc_score(dev_y, dev_predicted))
-dev_predictions_df = pd.DataFrame({
-    'instance': dev_ids,
-    'prediction': dev_predicted
-})
-dev_predictions_df.to_csv('lightgbm.pred', header=False, index=False, sep=" ")
+print('training per-user linear')
+train_y = np.array(train_y)
+train_x_withdf = sparse.hstack([train_x_sparse, 100*train_df[:, None]], format='csr')
+dev_x_withdf = sparse.hstack([dev_x_sparse, 100*dev_df[:, None]], format='csr')
+dev_predicted_indiv = np.zeros(dev_predicted.shape)
+for user in np.unique(train_users):
+    trainmask = (train_users == user)
+    devmask = (dev_users == user)
+    usermodel = LogisticRegression(C=.1)
+    usermodel.fit(train_x_withdf[trainmask, :], train_y[trainmask])
+    dev_predicted_indiv[devmask] = usermodel.predict_proba(dev_x_withdf[devmask, :])[:, 1]
+
+print('per-user auc:', roc_auc_score(dev_y, dev_predicted_indiv))
+
+
+# # train light gradient boosting machine model
+# d_train = lgb.Dataset(train_x_sparse, label=train_y)
+# d_valid = lgb.Dataset(dev_x_sparse, label=dev_y)
+# params = {
+#     'application': 'binary',
+#     'metric': 'auc',
+#     'learning_rate': .1,
+#     'num_leaves': 128,
+#     'min_data_in_leaf': 20,
+# }
+# bst = lgb.train(params, d_train, valid_sets=[d_train, d_valid],
+#                 valid_names=['train', 'valid'],
+#                 num_boost_round=1000, verbose_eval=10)
+# dev_predicted = bst.predict(dev_x_sparse)
+# # print auc score
+# print(roc_auc_score(dev_y, dev_predicted))
+# dev_predictions_df = pd.DataFrame({
+#     'instance': dev_ids,
+#     'prediction': dev_predicted
+# })
+# dev_predictions_df.to_csv('lightgbm.pred', header=False, index=False, sep=" ")
